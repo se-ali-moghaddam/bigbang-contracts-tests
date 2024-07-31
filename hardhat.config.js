763d7be5c6c7dea0dd5e7bffdaa15bcd2d007d1a
Ali_Moghaddam <se.ali.moghaddam@gmail.com>
2024-07-31 07:04:50 -0700

    Change Security Infos

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,8 @@ require('@nomicfoundation/hardhat-web3-v4');
 require("@nomiclabs/hardhat-solhint");
 
 const BNB_TESTNET_URL = 'https://go.getblock.io/a3a4ef15f64942ccac82ff568ed2edb7';
-const BNB_ACCOUNT_PRIVATE_KEY = '0f32ac67cea43b3a89e8bf1c36fe035b2931c36ea1dd41de8bf42f7f5c1e7e18';
-const BSCSCAN_API_KEY = '1DIYEA966GPR43UP3E7A9YKD7QGTT5D4KI';
+const BNB_ACCOUNT_PRIVATE_KEY = 'MyPrivateKey';
+const BSCSCAN_API_KEY = 'MyApiKey';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
